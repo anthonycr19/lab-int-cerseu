58b036bc8039996f57e5053672361e625585c5ca
“Anthony <acarrillorj@gmail.com>
2024-11-10 13:08:49 -0500

    add files urls and views

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def owner_list(request):
+    data_context = {
+        'nombre': "Katty"
+    }
+
+    return render(request, 'owner/owner_list.html', context={'data': data_context})
