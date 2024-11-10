611c9a4b610f8494321862b537fbd5b4a90b754a
“Anthony <acarrillorj@gmail.com>
2024-11-10 13:59:13 -0500

    add file html owner list

--- owner/urls.py
+++ owner/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('owner_list/', views.owner_list, name="owner_list")
+    path('owner_list/', views.owner_list, name="owner_list"),
+
+    path('owner_detail/<int:id_owner>', views.owner_detail, name="owner_detail")
 ]
--- owner/views.py
+++ owner/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def owner_list(request):
-    data_context = {
-        'nombre': "Katty"
-    }
+    # data_context = {
+    #     'nombre': "Katty",
+    #     'edad': 26,
+    #     'pais': "Perú"
+    # }
+
+    data_context = [
+        {
+            'nombre': "Katty",
+            'edad': 26,
+            'pais': "Perú"
+        },
+        {
+            'nombre': "Benito",
+            'edad': 22,
+            'pais': "España"
+        },
+        {
+            'nombre': "Marianela",
+            'edad': 23,
+            'pais': "Colombia"
+        }
+    ]
 
     return render(request, 'owner/owner_list.html', context={'data': data_context})
+
+
+def owner_detail(request, id_owner):
+    print("ID Owner: {}".format(id_owner))
+
+    return render(request, 'owner/owner_list.html', context={'data': id_owner})
+
