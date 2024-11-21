949887cda14b247bb2d236eea05a388b3c7571c7
“Anthony <acarrillorj@gmail.com>
2024-11-20 20:12:24 -0500

    update templates

--- owner/views.py
+++ owner/views.py
@@ -14,17 +14,36 @@ def owner_list(request):
         {
             'nombre': "Katty",
             'edad': 26,
-            'pais': "Perú"
+            'pais': "Perú",
+            'dni': '23231212',
+            'vigente': True,
+            'pokemons': [
+                {
+                    'nombre_p': 'Charizard',
+                    'ataques': ['Ataque 1 - Ch', 'Ataque 2 - Ch', 'Ataque 3 - Ch']
+                }
+            ]
         },
         {
             'nombre': "Benito",
             'edad': 22,
-            'pais': "España"
+            'pais': "España",
+            'dni': '88881212',
+            'vigente': False
         },
         {
             'nombre': "Marianela",
             'edad': 23,
-            'pais': "Colombia"
+            'pais': "Colombia",
+            'dni': '23239999',
+            'vigente': True
+        },
+        {
+            'nombre': "Carlos",
+            'edad': 17,
+            'pais': "Perú",
+            'dni': '77771212',
+            'vigente': True
         }
     ]
 
